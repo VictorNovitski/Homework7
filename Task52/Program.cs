c4e17973b17c7950b826dc9c22a126796b45be40
Victor N <vik214@yandex.ru>
2023-02-07 17:31:44 +0300

    задача 52

---
+++ Task52/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrica = new int[n, m];
+Generate2DArray(matrica);
+Print2DArray(matrica);
+
+void Generate2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return;
+}
+
+void Print2DArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < matrica.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrica.GetLength(0); i++)
+    {
+        avarage = (avarage + matrica[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
